59bc27b8cdc7490c29648a3105714e55e1476aeb
Avinash Raja <avinashraja98@gmail.com>
2018-02-11 15:53:01 -0330

    added functions and setup io pins

--- Toy_Car_Remote.ino
+++ Toy_Car_Remote.ino
@@ -12,31 +12,25 @@ const char index_page[] PROGMEM = R"=====(
 webpage
 )=====";
 
+const int inp1 = 5;
+const int inp2 = 4;
+
+void SetupAP();
+void SetupDNS();
+void SetupWebServer();
+void SetupIO();
+
 void ServeWebPage();
 
 void setup()
 {
-	Serial.begin(115200);
-	Serial.println();
+	Serial.begin(115200);	
 
-	Serial.print("Setting configuration... ");
-	Serial.println(WiFi.softAPConfig(local_IP, gateway, subnet) ? "Ready" : "Failed!");
-
-	Serial.print("Setting Access Point... ");
-	Serial.println(WiFi.softAP("Car") ? "Ready" : "Failed!");
-
-	Serial.print("IP address = ");
-	Serial.println(WiFi.softAPIP());
-
-	if (!MDNS.begin("car")) {
-		Serial.println("Error setting up MDNS responder!");
-	}
-	Serial.println("mDNS responder started");
-
-	server.on("/", ServeWebPage);
-	server.begin();
-	Serial.println("HTTP server started");
-
+	SetupAP();
+	SetupDNS();
+	SetupWebServer();
+	SetupIO();
+	
 }
 
 void loop() {
@@ -46,4 +40,37 @@ void loop() {
 void ServeWebPage() {
 	String s = index_page;
 	server.send(200, "text/html", s);
+}
+
+void SetupAP()
+{
+	Serial.print("Setting configuration... ");
+	Serial.println(WiFi.softAPConfig(local_IP, gateway, subnet) ? "Ready" : "Failed!");
+
+	Serial.print("Setting Access Point... ");
+	Serial.println(WiFi.softAP("Car") ? "Ready" : "Failed!");
+
+	Serial.print("IP address = ");
+	Serial.println(WiFi.softAPIP());
+}
+
+void SetupDNS()
+{
+	if (!MDNS.begin("car")) {
+		Serial.println("Error setting up MDNS responder!");
+	}
+	Serial.println("mDNS responder started");
+}
+
+void SetupWebServer()
+{
+	server.on("/", ServeWebPage);
+	server.begin();
+	Serial.println("HTTP server started");
+}
+
+void SetupIO()
+{
+	pinMode(inp1, OUTPUT);
+	pinMode(inp2, OUTPUT);
 }
